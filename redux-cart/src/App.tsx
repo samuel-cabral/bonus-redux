6326f8e3ed51b2b369bb7b4f3016a5eee3e2d396
Samuel Cabral <samuelcabral.mail@gmail.com>
2021-12-15 19:07:48 -0300

    Created Catalog component

--- redux-cart/src/App.tsx
+++ redux-cart/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import Catalog from './components/Catalog';
 
 import store from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <h1>Hello World</h1>
+      <Catalog />
     </Provider>
   );
 }
